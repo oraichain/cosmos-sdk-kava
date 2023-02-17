cb5047d6605e7ee08eead25d5a5f5f6e5ef7fdcb
drklee3 <derrick@dlee.dev>
2023-02-17 15:04:19 -0800

    Apply Kava patches 0004

---
+++ x/gov/types/tally_handler.go
@@ -0,0 +1,10 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// TallyHandler is the interface that is used for tallying votes.
+type TallyHandler interface {
+	Tally(sdk.Context, Proposal) (passes bool, burnDeposits bool, tallyResults TallyResult)
+}
